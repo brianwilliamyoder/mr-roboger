c282951cf871bc1ff669626e88e480802fab6554
Brian Yoder <brianyoder@gmail.com>
2022-11-17 22:56:01 -0800

    pass 2nd test

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,11 @@ function neighborhoodOutput(number) {
   parsedNumber = parseInt(number);
   outputNumbers = [];
   for (let i = 0; i < parsedNumber; i += 1) {
-    if (parsedNumber === 1) {
-      outputNumbers = "Beep!"
+    if (parsedNumber === 2) {
+      return "Boop!"
     }
-    return outputNumbers;
+    else if (parsedNumber === 1) {
+      return "Beep!"
+    }
   }
 }
