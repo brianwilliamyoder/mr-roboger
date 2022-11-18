4625f93bf052b86ccce79c08d6ceb1a77ee0b188
Brian Yoder <brianyoder@gmail.com>
2022-11-17 23:06:45 -0800

    pass 3rd test

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
 function neighborhoodOutput(number) {
-  parsedNumber = parseInt(number);
+  parsedNumber = parseInt(number).split();
   outputNumbers = [];
   for (let i = 0; i < parsedNumber; i += 1) {
-    if (parsedNumber === 2) {
+    if (parsedNumber === 3) {
+      return "Won't you be my neighbor?"
+    }
+   else if (parsedNumber === 2) {
       return "Boop!"
     }
     else if (parsedNumber === 1) {
